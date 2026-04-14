bc72bcdf3b90b983e79c258a71dcf243ea109784
Melina <melina.khettab@universite-paris-saclay.fr>
2026-04-14 15:29:37 +0200

    Initial structure project

---
+++ shared/types/user.types.ts
@@ -0,0 +1,26 @@
+﻿export interface User {
+  id: number
+  email: string
+  createdAt: string
+}
+
+export interface Profile {
+  id: number
+  userId: number
+  age: number
+  weight: number
+  height: number
+  objective: Objective
+  level: Level
+  imc?: number
+  bmr?: number
+  tdee?: number
+}
+
+export type Objective = 'perte_poids' | 'prise_masse' | 'remise_en_forme' | 'maintien' | 'performance'
+export type Level = 'debutant' | 'intermediaire' | 'avance'
+
+export interface AuthResponse {
+  token: string
+  user: User
+}
